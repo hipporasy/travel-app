6efb5c3f59831b784949c4dbd0b17ce67c0abe94
Marasy Phi <hipporasy@gmail.com>
2021-01-30 11:47:28 +0700

    Initial Commit

---
+++ Triplr/TriplrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriplrApp.swift
+//  Triplr
+//
+//  Created by Marasy Phi on 30/1/21.
+//
+
+import SwiftUI
+
+@main
+struct TriplrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
